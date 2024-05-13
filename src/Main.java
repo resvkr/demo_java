db4008e44e597cb3031fb75ec6079ef2078d7280
Zenia <eugeniababak@gmail.com>
2024-05-13 18:14:47 +0200

    tables and circle

--- src/Main.java
+++ src/Main.java
@@ -39,7 +39,7 @@ public class Main {
 // таблиця квардратів
     // draw a circle using just * not in array
     public static void main(String[] args) {
-        int num = 10;
+        int num = 4;
         int[][] numsArr = new int[num][num];
 
         for (int i = 0; i < numsArr.length; i++) {
@@ -49,10 +49,46 @@ public class Main {
                 numsArr[i][j] = (i + 1) * (j + 1);
             }
         }
+        showArray(numsArr);
+        System.out.println();
 
+        for (int i = 0; i < numsArr.length; i++) {
+
+            for (int j = 0; j < numsArr.length; j++) {
+                double temp = i * 10 + j;
+                numsArr[i][j] = (int) Math.pow(temp, 2);
+            }
+        }
 
         showArray(numsArr);
+        int counter = num - 1;
+
+        for (int i = 1; i <= num; i++) {
+            for (int j = 0; j < num - i; j++) {
+                System.out.print("  ");
+            }
+            for (int j = 0; j < i * 2; j++) {
+                System.out.print("+ ");
+
+            }
+            System.out.println();
+
+        }
+        for (int i = num; i >= 1; i--) {
+            for (int j = 0; j < num - i; j++) {
+                System.out.print("  ");
+            }
+            for (int j = 0; j < i * 2; j++) {
+                System.out.print("+ ");
+
+            }
+            System.out.println();
+
+        }
+
+
     }
+
 
     static void showArray(int[][] arr) {
         for (int i = 0; i < arr.length; i++) {
