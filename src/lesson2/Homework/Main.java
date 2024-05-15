d7f9f68ae1c2270c45fa384cd45680a5029d5837
nhavronskyi <nazar.havronskyi@dataart.com>
2024-05-15 18:59:27 +0200

    homework task

---
+++ src/lesson2/Homework/Main.java
@@ -0,0 +1,24 @@
+package lesson2.Homework;
+
+
+/**
+ * TODO:
+ *  - learn about ArrayList: https://www.w3schools.com/java/java_arraylist.asp
+ *  - Products Shop:
+ *      - class Shop
+ *          - product(quantities): apples, peaches, bananas
+ *          - showAllProducts
+ *      - class Customer
+ *          - product(quantities): apples, peaches, bananas
+ *          - methods
+ *              - showChosenProducts
+ *              - get product (shop - (num product))
+ *              - return product (shop + (num product))
+ *              Please take a look at exceptions when products are not available
+ *      task:
+ *          - play with arraylist (not too much)
+ *          - Please implement all methods and use it for two Customers
+ */
+
+public class Main {
+}
